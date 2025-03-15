e202358c02138eda653241d4ba5e8001c2ead6ca
Henrique Barrel <henriquebarrel@gmail.com>
2025-03-15 10:38:58 -0300

    Move chat logic to chat go file.

--- src/chat.go
+++ src/chat.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
 	"time"
+
+	"github.com/coder/websocket"
+	"github.com/coder/websocket/wsjson"
 )
 
 type User struct {
@@ -17,4 +24,52 @@ type chatServer struct {
 type response struct {
 	Message   string    `json:"message"`
 	Timestamp time.Time `json:"timestamp"`
+}
+
+func createServer() http.HandlerFunc {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c, err := websocket.Accept(w, r, &websocket.AcceptOptions{OriginPatterns: []string{"localhost:1234"}})
+
+		if err != nil {
+			fmt.Printf("An error occured: %v\n", err)
+		}
+
+		defer c.CloseNow()
+
+		ctx, cancel := context.WithCancel(r.Context())
+
+		defer cancel()
+
+		var v interface{}
+
+		for {
+			err = wsjson.Read(ctx, c, &v)
+
+			if err != nil {
+				fmt.Printf("Error when reading message: %v\n", err)
+				break
+			}
+
+			var responseMessage string
+
+			if v == "Connected to Websocket." {
+				responseMessage = "Successfully connected, hello!"
+			} else {
+				responseMessage = "Received! hello form server."
+			}
+
+			err = wsjson.Write(ctx, c, response{responseMessage, time.Now()})
+
+			log.Printf("Received: %v\n", v)
+
+			if err != nil {
+				fmt.Printf("Error while writing back to WebSocket: %v\n", err)
+				break
+			}
+		}
+
+		c.Close(websocket.StatusNormalClosure, "")
+	})
+
+	return handler
 }
